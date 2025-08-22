4ec25fe351787db2a30aeedd94f909abbb91ffba
Philip Jägenstedt <philip@foolip.org>
2025-08-22 08:10:37 +0200

    Align CSV header names with chromestatus.com docs
    The import feature was added here:
    https://github.com/GoogleChrome/chromium-dashboard/pull/5324
    https://github.com/GoogleChrome/chromium-dashboard/pull/5345

--- export.py
+++ export.py
@@ -14,13 +14,13 @@ def main():
     for item in data:
         if item['review_status'] == 'accept':
             rows.append({
-                'id': item['chromestatus_id'],
-                'web_feature': item['web_features_id'],
+                'Chrome Status Entry': item['chromestatus_id'],
+                'Feature ID': item['web_features_id'],
             })
 
     if rows:
         with open(MAPPING_EXPORT_FILE, 'w') as csvfile:
-            fieldnames = ['id', 'web_feature']
+            fieldnames = ['Chrome Status Entry', 'Feature ID']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(rows)
