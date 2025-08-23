d2ebd779010650a656d38d1e87753772fd777a47
Philip Jägenstedt <philip@foolip.org>
2025-08-23 20:47:44 +0200

    Re-check IDs during export

--- export.py
+++ export.py
@@ -1,9 +1,18 @@
 import json
 import csv
 
+from update import CHROMESTATUS_FILE, WEB_FEATURES_FILE
 from review import MAPPING_REVIEW_FILE
 
 MAPPING_EXPORT_FILE = "mapping-export.csv"
+
+with open(CHROMESTATUS_FILE) as f:
+    CHROMESTATUS = json.load(f)
+
+CHROMESTATUS_BY_ID = {str(entry["id"]): entry for entry in CHROMESTATUS}
+
+with open(WEB_FEATURES_FILE) as f:
+    WEB_FEATURES = json.load(f)
 
 
 def main():
@@ -17,6 +26,17 @@ def main():
     data.sort(key=lambda x: x["web_features_id"])
 
     for item in data:
+        # Skip invalid IDs and no-op changes.
+        entry = CHROMESTATUS_BY_ID.get(str(item["chromestatus_id"]))
+        if not entry:
+            continue
+        id = item["web_features_id"]
+        if id not in WEB_FEATURES["features"]:
+            continue
+        if entry["web_feature"] == id:
+            continue
+
+        # Export accepted mappings.
         if item["review_status"] == "accept":
             rows.append(
                 {
