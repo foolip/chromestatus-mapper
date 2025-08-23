c6e5948fb78250e42bdc095fcf48bdef66f9de9a
Philip Jägenstedt <philip@foolip.org>
2025-08-23 20:46:58 +0200

    Run ruff format

--- export.py
+++ export.py
@@ -3,7 +3,8 @@ import csv
 
 from review import MAPPING_REVIEW_FILE
 
-MAPPING_EXPORT_FILE = 'mapping-export.csv'
+MAPPING_EXPORT_FILE = "mapping-export.csv"
+
 
 def main():
     with open(MAPPING_REVIEW_FILE) as f:
@@ -16,15 +17,17 @@ def main():
     data.sort(key=lambda x: x["web_features_id"])
 
     for item in data:
-        if item['review_status'] == 'accept':
-            rows.append({
-                'Chrome Status Entry': item['chromestatus_id'],
-                'Feature ID': item['web_features_id'],
-            })
+        if item["review_status"] == "accept":
+            rows.append(
+                {
+                    "Chrome Status Entry": item["chromestatus_id"],
+                    "Feature ID": item["web_features_id"],
+                }
+            )
 
     if rows:
-        with open(MAPPING_EXPORT_FILE, 'w') as csvfile:
-            fieldnames = ['Chrome Status Entry', 'Feature ID']
+        with open(MAPPING_EXPORT_FILE, "w") as csvfile:
+            fieldnames = ["Chrome Status Entry", "Feature ID"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(rows)
